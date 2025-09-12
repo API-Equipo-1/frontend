b4f7913e3c9d6e61d482e3cf2357bba1d02e66ca
Antonella <antoh2003@gmail.com>
2025-09-12 19:30:54 -0300

    Added catalog

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Catalog } from './components/Catalog'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Catalog } from "./components/Catalog";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Product } from "./components/Product";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Catalog/>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Catalog />} />
+        <Route path="/product/:id" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,30 +1,27 @@
 import { useState, useEffect } from "react";
-import './Catalog.css'
+import "./Catalog.css";
 import { ProductCard } from "./ProductCard";
 
 export const Catalog = () => {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        fetch('/products.json')
-            .then(response => response.json())
-            .then(data => setProducts(data.sort((a, b) => a.name.localeCompare(b.name))))
-            .catch(error => console.error('Error fetching products:', error));
-    }, [])
+  useEffect(() => {
+    fetch("/products.json")
+      .then((response) => response.json())
+      .then((data) =>
+        setProducts(data.sort((a, b) => a.name.localeCompare(b.name)))
+      )
+      .catch((error) => console.error("Error fetching products:", error));
+  }, []);
 
-    return (
-        <div >
-            <h1>Cátalogo de productos</h1>
-
-            <div className="product-list">
-                {
-                    products.map(
-                        product => (
-                            <ProductCard key={product.id} {...product} />
-                        )
-                    )
-                }
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Cátalogo de productos</h1>
+      <div className="product-list">
+        {products.map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
+      </div>
+    </div>
+  );
+};
